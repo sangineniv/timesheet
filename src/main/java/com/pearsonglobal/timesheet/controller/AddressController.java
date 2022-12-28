71ec36bb0ed96558d8912a35afe4a1920d9dbbf4
sangineniv <sangineniv@gmail.com>
2022-12-28 15:07:35 -0600

    "created address controller"

--- src/main/java/com/pearsonglobal/timesheet/controller/AddressController.java
+++ src/main/java/com/pearsonglobal/timesheet/controller/AddressController.java
@@ -1,4 +1,4 @@
-package com.pearsonglobal.timesheet.addresscontroller;
+package com.pearsonglobal.timesheet.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
