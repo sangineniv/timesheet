3762a7ad7ce6fe793b296551b16422ef5c405e45
Krishna2320 <krishnaa2320@gmail.com>
2023-01-03 16:58:58 -0600

    Classroom Features Implementation

---
+++ src/main/java/com/pearsonglobal/timesheet/dto/ClassroomDto.java
@@ -0,0 +1,35 @@
+package com.pearsonglobal.timesheet.dto;
+
+public class ClassroomDto {
+
+    private int numberOfStudents;
+
+    private int numberOfAdvisors;
+
+    private int numberOfProfessors;
+
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
+
+    public void setNumberOfStudents(int numberOfStudents) {
+        this.numberOfStudents = numberOfStudents;
+    }
+
+    public int getNumberOfAdvisors() {
+        return numberOfAdvisors;
+    }
+
+    public void setNumberOfAdvisors(int numberOfAdvisors) {
+        this.numberOfAdvisors = numberOfAdvisors;
+    }
+
+    public int getNumberOfProfessors() {
+        return numberOfProfessors;
+    }
+
+    public void setNumberOfProfessors(int numberOfProfessors) {
+        this.numberOfProfessors = numberOfProfessors;
+    }
+
+}
