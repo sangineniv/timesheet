64b3b9041812df4f7f718c347ef667b1c23b4f04
Nikhithaguguloth <nikki.guguloth@gmail.com>
2023-01-09 22:04:25 -0500

    "advisor controller changes"

--- src/main/java/com/pearsonglobal/timesheet/dao/AddressDao.java
+++ src/main/java/com/pearsonglobal/timesheet/dao/AddressDao.java
@@ -1,15 +1,16 @@
 package com.pearsonglobal.timesheet.dao;
 
 
+import com.pearsonglobal.timesheet.dto.PersonDto;
 import org.springframework.stereotype.Repository;
+
 @Repository
 public class AddressDao {
 
-    public void AddressDao(){
-
-    }
-
-    public void address(){
-
+    public PersonDto find(){
+        PersonDto personDto1=new PersonDto();
+        personDto1.setId(3);
+        personDto1.setName("alan");
+        return personDto1;
     }
         }
